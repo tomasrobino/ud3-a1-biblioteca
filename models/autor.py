c74dbc2101e48618eca87100c3186779da63384f
Tomas Robino <tomaserobino@gmail.com>
2026-02-19 19:08:18 +0100

    autro

--- models/autor.py
+++ models/autor.py
@@ -2,8 +2,8 @@ from odoo import models, fields, api
 
 
 class autor(models.Model):
-    _name = 'biblioteca.libro'
-    _description = 'biblioteca.libro'
+    _name = 'biblioteca.autor'
+    _description = 'biblioteca.autor'
     name = fields.Char()
     nationality = fields.Char()
     birthDate = fields.Date()
