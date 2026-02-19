1a24173230a153f014a1c180341cfd3607f3e12d
Tomas Robino <tomaserobino@gmail.com>
2026-02-19 19:43:07 +0100

    author views

--- __manifest__.py
+++ __manifest__.py
@@ -22,7 +22,9 @@ Long description of module's purpose
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/autor_view.xml',
+        'views/libro_view.xml',
+        'views/menu_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
